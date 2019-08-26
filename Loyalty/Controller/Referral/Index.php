d6d1ce97847136ae79c2fe2993ce780a8b132054
Eyal C <eyal.chistik@gmail.com>
2019-08-26 11:55:53 +0300

    changing folder and file names for better consistency and clarity

--- Loyalty/Controller/Referral/Index.php
+++ Loyalty/Controller/Referral/Index.php
@@ -18,7 +18,7 @@ class Index extends \Magento\Framework\App\Action\Action
     public function execute()
     {
         $resultPage = $this->resultPageFactory->create();
-        $resultPage->getConfig()->getTitle()->prepend(__('Refer'));
+        $resultPage->getConfig()->getTitle()->prepend(__('Referral'));
         return $resultPage;
     }
 }
